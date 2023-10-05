a83671489dbe84f974944bb0497be5e52038c1ac
ivan295 <rafaelivan1995@hotmail.com>
2023-10-05 16:06:09 -0500

    commit inicial

---
+++ database/migrations/2023_10_05_173520_create_species_animal_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSpeciesAnimalTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('species_animal', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->boolean('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('species_animal');
+    }
+}
